34913c16d5764226e1d10c2c39e1d8446643595f
Anaysa Lopes <anaysalopees@gmail.com>
2025-07-28 16:13:35 -0300

    feat: início do formulário com título e descrição

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+  const [titulo, setTitulo] = useState('')
+  const [descricao, setDescricao] = useState('')
+
   return (
-    <>
-      <div>
-       <h1>Projetos Iniciados</h1>
-      </div>
-    </>
+    <div className="container">
+      <h1>Painel de Gerenciamento</h1>
+
+      <form className="form">
+        <h3 className="subtitulo">Novo Post</h3>
+
+        <div className="form-control">
+          <label htmlFor="title">Título</label>
+          <input
+            id="title"
+            type="text"
+            value={titulo}
+            onChange={(e) => setTitulo(e.target.value)}
+            placeholder="Digite o título"
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="description">Descrição</label>
+          <input
+            id="description"
+            type="text"
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
+            placeholder="Digite a descrição"
+          />
+        </div>
+
+      </form>
+    </div>
   )
 }
 
