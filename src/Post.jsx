fd0259c93c0af8797bff59487953f51d6572f195
Anaysa Lopes <anaysalopees@gmail.com>
2025-08-04 14:39:47 -0300

    feat: adiciona suporte a props no componente Post

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,16 +1,24 @@
-function Post({ titulo, descricao, categoria, dataPublicaco, urlImagem, onDelete }) {
-    return (
-        <div className="post-card">
-            <img src={urlImagem} alt="Imagem do post" className="post-image" />
-            <div className="post-content">
-                <span className="post-category">{categoria.toUpperCase()}</span>
-                <h2>{titulo}</h2>
-                <p>{descricao}</p>
-                <p className="post-date">Publicado em: {dataPublicaco}</p>
-                <button className="delete-button" onClick={onDelete}>Excluir</button>
-            </div>
-        </div>
-    )
+import './Post.css'
+
+function Post({ tipo, titulo, descricao, data }) {
+  return (
+    <div className="post-card">
+      <img
+            src="https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2023/06/inteligencia-artificial.webp"
+            alt="Imagem do post"
+            className="post-image"
+       />
+
+
+      <div className="post-content">
+        <span className="post-category">{tipo?.toUpperCase()}</span>
+        <h2>{titulo}</h2>
+        <p>{descricao}</p>
+        <p className="post-date">Publicado em: {data}</p>
+        <button className="delete-button">Excluir</button>
+      </div>
+    </div>
+  )
 }
 
 export default Post
